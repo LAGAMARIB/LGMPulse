97a4cd432574b94d871714924591beff4c2ce3e6
Alexandre <xanderib@gmail.com>
2025-12-08 21:17:31 -0300

    Tela de digitação de valores

---
+++ LGMPulse.WebApp/Controllers/LancamentoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LGMPulse.WebApp.Controllers;
+
+public class LancamentoController : Controller
+{
+    public IActionResult NovaReceita()
+    {
+        return View();
+    }
+
+    public IActionResult NovaDespesa()
+    {
+        return View();
+    }
+
+    public IActionResult DigitarValor()
+    {
+        return View();
+    }
+}
